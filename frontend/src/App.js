76eed3e02fdfc49c11aaf38422e5990df24124a1
bastianarmananta <92047013+bastianarmananta@users.noreply.github.com>
2021-10-12 00:46:06 +0700

    (feat) added upload form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,20 @@
-import axios from "axios";
 import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const baseURL = "http://localhost:8000/predict";
+import UploadFiles from "./components/upload-files.component";
 
-export default function App() {
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(`${baseURL}/1`).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  function createPost() {
-    axios
-      .post(baseURL, {
-        title: "Hello World!",
-        body: "This is a new post."
-      })
-      .then((response) => {
-        setPost(response.data);
-      });
-  }
-
-  if (!post) return "No post!"
-
+function App() {
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-      <button onClick={createPost}>Create Post</button>
+    <div className="container" style={{ width: "600px" }}>
+      <div style={{ margin: "20px" }}>
+        <h3>bezkoder.com</h3>
+        <h4>React upload Files</h4>
+      </div>
+
+      <UploadFiles />
     </div>
   );
 }
+
+export default App;
